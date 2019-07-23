23ea46f8cfac0c659b075fcda51c9257d415e23f
Michael A. Chapman <chapmanm305@gmail.com>
2019-07-23 14:18:22 -0700

    Implement color change on button press

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -4,13 +4,27 @@ import { View, Text, TouchableOpacity, Alert, StyleSheet } from 'react-native';
 import { LocationContext } from './context/locationsContext';
 
 export default class HomeScreen extends Component {
+  constructor() {
+
+    super();
+    this.state = { ColorHolder: '#00BCD4' }
+  }
+
+  ChangeColorFunction = () => {
+
+    var ColorCode = 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')';
+    this.setState({
+      ColorHolder: ColorCode
+    })
+  }
+
   render() {
     return (
       <LocationContext.Consumer>
         {state => {
           return (
-            <View style={styles.container}>
-              <TouchableOpacity onPress={state.findCurrentLocation}>
+            <View style={[styles.container, { backgroundColor: this.state.ColorHolder }]}>
+              <TouchableOpacity onPress={() => { state.findCurrentLocation(); this.ChangeColorFunction() }} >
               <Text> Where am I? </Text>
               <Text> {state.latitude} </Text>
               <Text> {state.longitude} </Text>
@@ -26,7 +40,7 @@ export default class HomeScreen extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'lightblue',
+    // backgroundColor: 'lightblue',
     alignItems: 'center',
     justifyContent: 'center'
   },
