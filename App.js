417537d7b39ef3a9e403f705b7d2719bb9ca1120
Michael A. Chapman <chapmanm305@gmail.com>
2019-07-23 10:55:59 -0700

    Implement geolocation finding on HomeScreen

--- App.js
+++ App.js
@@ -44,7 +44,8 @@ async function loadResourcesAsync() {
   ]);
 }
 
-function handleLoadingError(error: Error) {
+function handleLoadingError() {
+  let error = 'error';
   // In this case, you might want to report the error to your error reporting
   // service, for example Sentry
   console.warn(error);
@@ -58,5 +59,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
 });
---
+++ screens/context/locationsContext.js
@@ -0,0 +1,23 @@
+// import React from "react";
+
+// export const LocationContext = React.createContext();
+
+// export default class LocationProvider extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       location: '',
+//       setLocation: (location) => {
+//         this.setState({ location });
+//       }
+//     };
+//   }
+
+//   render() {
+//     return (
+//       <LocationContext.Provider value={this.state}>
+//         {this.props.children}
+//       </LocationContext.Provider>
+//     );
+//   }
+// }
